c69d19a6269272670cd0544b250c30e255d96a93
rgarciacejudo <rgarcia@imac-de-ricardo.gateway.2wire.net>
2014-09-24 10:40:01 -0500

    pendientes

---
+++ InvestigationProject/app/Model/Section.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+App::uses('AppModel', 'Model');
+
+/**
+ * Modelo para la tabla sections
+ *
+ * @author rgarcia
+ */
+class Section extends AppModel {
+
+    public $hasMany = 'SectionsField';
+    
+    public $validate = array(
+        'name' => array(
+            'required' => array(
+                'rule' => array('notEmpty'),
+                'message' => 'El nombre de la sección es requerido.'
+            ),
+            'unique' => array(
+                'rule' => 'isUnique',
+                'message' => 'El nombre de la sección ingresado ya existe.'
+            )
+        ),
+        'description' => array(
+            'required' => array(
+                'rule' => array('notEmpty'),
+                'message' => 'La descripción de la sección es requerida.'
+            )
+        )
+    );       
+
+}
